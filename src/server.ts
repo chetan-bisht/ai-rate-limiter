baed89cb403cb10c36147383f83b61656bb6b06c
Chetan Bisht <chetanbisht6113@gmail.com>
2026-02-09 17:50:02 +0530

    Add server.ts file in src/ and minor fixes in the tsconfig & package.json.

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+
+import redis from './config/redis.js';
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+
+  await redis.set('test_key', 'Hello from Docker!');
+  const value = await redis.get('test_key');
+  
+  res.json({
+    message: 'API Rate Limiter System',
+    redis_status: value ? 'Connected & Working' : 'Failed',
+    value_from_redis: value
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
