ef1146bcce018b68c526fda13d55a907806c6918
Chetan Bisht <chetanbisht6113@gmail.com>
2026-02-10 13:28:58 +0530

    Update server.ts.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-
 import redis from './config/redis.js';
+import { rateLimiter } from './middleware/rateLimiter.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(rateLimiter);
 
 app.get('/', async (req, res) => {
 
